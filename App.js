2e142fb79221849871efb8ad2f5c176eb84343f3
Arun Rajan <arunrajan@MacBook-Pro.local>
2018-08-17 22:58:03 +0530

    calculator layout

--- App.js
+++ App.js
@@ -1,32 +1,35 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import InputNumberButton from './InputNumberButton';
 
-type Props = {};
-export default class App extends Component<Props> {
+const buttons = [
+  ['CLEAR', 'DEL'],
+  [7, 8, 9, '/'],
+  [4, 5, 6, 'x'],
+  [1, 2, 3, '-'],
+  [0, '.', '=', '+'],
+]
+export default class App extends Component {
+
+  renderButtons() {
+    let layouts = buttons.map((buttonRows, index) => {
+      let rowItem = buttonRows.map((buttonItems, buttonIndex) => {
+        return <InputNumberButton key={'btn-' + buttonIndex} />
+      })
+      return <View style={styles.inputRow} key={'row-' + index}>{rowItem}</View>
+    });
+   return layouts
+  }
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.resultContainer}>
-          <Text style={styles.resultText}>0</Text>
+          <Text style={styles.resultText}> 0 </Text>
         </View>
 
         <View style={styles.inputContainer}>
-        
+              {this.renderButtons()}
         </View>
       </View>
     );
@@ -53,4 +56,8 @@ const styles = StyleSheet.create({
     padding: 20,
     textAlign: 'right'
   },
+  inputRow: {
+    flex: 1,
+    flexDirection: 'row',
+  }
 });
